9d8f78d88167558817c9caedc3e340e045b1646b
jaeunjung <jju0700@gmail.com>
2026-01-29 02:09:05 +0900

    [2026-01-29] style(ui): 푸터 문구 수정 및 사용자 메뉴 정리
    * 변경 사항: 푸터 슬로건 변경 및 사용자 메뉴에서 '저장한 장소' 항목 제거
    
    * 이유/배경: 브랜드 메시지 개선 및 사용하지 않는 메뉴 삭제
    
    * 영향 범위: components.js 및 전체 HTML 페이지

--- js/components.js
+++ js/components.js
@@ -85,12 +85,6 @@ class ComponentLoader {
                                     </svg>
                                     내 여행 계획
                                 </a>
-                                <a href="#" class="user-dropdown-item">
-                                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-                                    </svg>
-                                    저장한 장소
-                                </a>
                                 <a href="recommend.html" class="user-dropdown-item">
                                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                         <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
@@ -140,7 +134,7 @@ class ComponentLoader {
                     <a href="index.html" class="footer__logo">
                         <img src="images/logo.png" alt="틈새 Teumsae" class="logo-img" style="height: 40px;">
                     </a>
-                    <p class="footer__desc">서울의 숨은 여유를 디자인하다</p>
+                    <p class="footer__desc">서울의 틈새, 당신만의 여유를 찾다</p>
                     <div class="footer__links">
                         <a href="explore.html">장소 탐색</a>
                         <a href="recommend.html">여행지 추천</a>
